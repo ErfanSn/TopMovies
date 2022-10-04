123b0bc1aa07b2a6f4d7749e9c67cf24c3c5899a
ErfanSn <50053121+ErfanSn@users.noreply.github.com>
2022-10-04 19:59:32 +0330

    Remove system windows insets

--- app/src/main/java/ir/erfansn/topmovies/MainActivity.kt
+++ app/src/main/java/ir/erfansn/topmovies/MainActivity.kt
@@ -3,11 +3,13 @@ package ir.erfansn.topmovies
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
 import ir.erfansn.topmovies.features.home.HomeScreen
 import ir.erfansn.topmovies.ui.theme.TopMoviesTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
         setContent {
             TopMoviesTheme {
